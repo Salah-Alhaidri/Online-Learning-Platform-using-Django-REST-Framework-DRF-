216f9c01a2cb02baf4315083db73bc488bc8034b
salah <salah.alhaidri@gmail.com>
2025-01-06 17:52:03 +0300

    Install Dependencis

--- SkillStream/permissions.py
+++ SkillStream/permissions.py
@@ -6,9 +6,6 @@ class IsInstructorOrReadOnly(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        # Allow read-only access for all requests
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        # Allow write access only if the user is an instructor
         return request.user.profile.is_instructor
